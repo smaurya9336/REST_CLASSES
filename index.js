b67d56e94b815bebb34d516a4cfa40780c0b7447
Sarita <sm7767119@gmail.com>
2024-04-22 22:36:02 +0530

    delete rest

--- index.js
+++ index.js
@@ -69,6 +69,12 @@ app.get("/posts/:id/edit", (req, res) => {
   res.render("edit.ejs", { post });
 });
 
+app.delete("/posts/:id", (req, res) => {
+  let { id } = req.params;
+  posts = posts.filter((p) => id !== p.id);
+  res.redirect("/posts");
+});
+
 app.listen(port, () => {
   console.log(`listening to port : ${port}`);
 });
