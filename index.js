abd39adbf96dc57113bb31ba40fba8023b65c697
Sarita <sm7767119@gmail.com>
2024-04-22 19:42:33 +0530

    Edit routs

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const methodOverride = require("method-override");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
+
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
